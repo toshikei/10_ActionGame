e3b7a4ab1b1b699bf2afc4092743cb81f2f467f6
Rinako Fujisawa <morijyobi4153211@gmail.com>
2016-10-19 10:25:33 +0900

    first commit

---
+++ src/resource.js
@@ -0,0 +1,11 @@
+var res = {
+   bat_frames: "res/bat_frames.png",
+   leftbutton_png: "res/leftbutton.png",
+   rightbutton_png: "res/rightbutton.png",
+
+};
+
+var g_resources = [];
+for (var i in res) {
+   g_resources.push(res[i]);
+}
